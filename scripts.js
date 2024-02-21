077b5bb97bf5e8ce0b1d4d954d8b7b0ad8ab9941
AdrianTerciado <terciadojano@gmail.com>
2024-02-21 06:20:09 +0100

    Terminado, aunque el punto 7 hay que hacerlo de otra forma

--- scripts.js
+++ scripts.js
@@ -1,1 +1,77 @@
 console.log(document.title);
+
+/** 1. Cambia el título "Generation 1 Pokémon" por "Generasión 1 Pokimon". */
+document.querySelector("#gen-1").innerHTML = "Generasión 1 Pokimon";
+
+/** 2. Cambia el color de fondo de la primera generación de Pokimon. */
+document.querySelector(".infocard-list").style.background = "yellow";
+
+/** 3. Imprime por consola la URL de la página. */
+console.log(window.location.href);
+
+/** 4. Imprime por consola el dominio de la página. */
+
+console.log(window.location.href.length);
+
+let dominio = [];
+
+for (let i = window.location.href.length-1; i >= 0; i--) {
+     
+    dominio.unshift(window.location.href[i]);
+    
+    if (window.location.href[i] == ".") {
+        break;
+    }
+
+    console.log(window.location.href[i]);
+}
+
+console.log(dominio.join());
+
+console.log(".h,t,m,l".replaceAll(",", ""));
+
+console.log(window.location.host);
+
+/** 5. Imprime todos los nodos de imagen. */
+
+console.log(document.querySelectorAll("img"));
+
+
+/** 6. Sustituye el atributo "src" de todas las imágenes por este "https://media.giphy.com/media/2v170e71aanfi/giphy.gif" */
+
+let imagenes = document.querySelectorAll("img");
+
+ for (let i=0; i<imagenes.length; i++) {
+    imagenes[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+ }
+
+
+ /** 7. Cambia el fondo de todos los infocard-lg-data  para todos los Pokimon voladores  flying */
+
+ /* let document.querySelectorAll(".infocard-lg-data .text-muted > .itype flying") = blue;*/
+
+ function cambiarColor() {
+    let elements = document.querySelectorAll(".infocard-lg-data.text-muted")
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].innerHTML.includes("itype flying")) {
+            elements[i].style.background = "red";
+        }
+    }
+}
+
+cambiarColor();
+ 
+ 
+/*  function cambiarColor() {
+    
+    let elements = document.querySelectorAll(".infocard-lg-data.text-muted")
+    for (let i = 0; i < elements.length; i++) {
+     //   if (elements[i].innerHTML.includes("itype flying")) {
+        const el=elements[i].querySelector(".flying");
+        if (el!==undefined) {
+            el.style.background = "red";
+        }
+    }
+}
+
+cambiarColor(); */
